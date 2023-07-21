54735f60426e17d7ad071f21778ed600f82dd23a
Kenneth Davidson <xylene2016@protonmail.com>
2023-07-21 11:53:38 -0400

    Update nghttp3 and ngtcp2

--- el7/build-curl.sh
+++ el7/build-curl.sh
@@ -271,7 +271,7 @@ _build_brotli() {
         LDFLAGS='' ; LDFLAGS="${_ORIG_LDFLAGS}"' -Wl,-rpath,\$$ORIGIN' ; export LDFLAGS
         ./configure \
         --build=x86_64-linux-gnu --host=x86_64-linux-gnu \
-        --enable-shared --enable-static \
+        --enable-shared --disable-static \
         --prefix=/usr --libdir=/usr/lib64 --includedir=/usr/include --sysconfdir=/etc
         make all
         rm -fr /tmp/brotli
@@ -1813,7 +1813,7 @@ _dl_nghttp3() {
     set -e
     cd /tmp
     rm -fr /tmp/nghttp3
-    git clone --recursive -b 'v0.11.0' 'https://github.com/ngtcp2/nghttp3.git'
+    git clone --recursive -b 'v0.13.0' 'https://github.com/ngtcp2/nghttp3.git'
     rm -fr nghttp3/.git
     sleep 2
     tar -zcf nghttp3-git.tar.gz nghttp3
@@ -1826,7 +1826,7 @@ _dl_ngtcp2() {
     set -e
     cd /tmp
     rm -fr /tmp/ngtcp2
-    git clone --recursive -b 'v0.15.0' 'https://github.com/ngtcp2/ngtcp2.git'
+    git clone --recursive -b 'v0.17.0' 'https://github.com/ngtcp2/ngtcp2.git'
     rm -fr ngtcp2/.git
     sleep 2
     tar -zcf ngtcp2-git.tar.gz ngtcp2
