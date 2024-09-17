a10b8f7f608e5ebbeb5d139b30beeb514bb7502a
Kenneth Davidson <xylene2016@protonmail.com>
2024-09-17 08:53:01 -0400

    Update build-curl.sh

--- ub2204/build-curl.sh
+++ ub2204/build-curl.sh
@@ -95,7 +95,8 @@ _build_gmp() {
     _tmp_dir="$(mktemp -d)"
     cd "${_tmp_dir}"
     _gmp_ver="$(wget -qO- 'https://gmplib.org/download/gmp/' | grep -i 'gmp-[0-9]' | sed -e 's|"|\n|g' | grep -i '^gmp-[0-9].*xz$' | sed -e 's|gmp-||g' -e 's|\.tar.*||g' | sort -V | tail -n 1)"
-    wget -c -t 9 -T 9 "https://gmplib.org/download/gmp/gmp-${_gmp_ver}.tar.xz"
+    #wget -c -t 9 -T 9 "https://gmplib.org/download/gmp/gmp-${_gmp_ver}.tar.xz"
+    wget 'https://gmplib.org/download/gmp/gmp-6.3.0.tar.xz'
     tar -xof gmp-*.tar*
     sleep 1
     rm -f gmp-*.tar*
